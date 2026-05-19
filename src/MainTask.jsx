5a735ffc61fdf4fcd3f8c4b65c61b3fa477ab377
Karthickbala-Java <karthikbala769@gmail.com>
2026-05-19 20:16:18 +0530

    feat:display those inputs

--- src/MainTask.jsx
+++ src/MainTask.jsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import ToDoList from "./ToDoList";
 
 export default function MainTask() {
-  let arr = [
+  let arr1 = [
     "Learn React useReducer in depth",
     "Build a task manager app",
     "practice React componet",
     "Build a small project",
     "deploy project on netlify",
   ];
+  const [value, setValue] = useState("");
+  let [arr, setArr] = useState(arr1);
   return (
     <div className="main-task">
       <div className="title">
@@ -16,8 +18,27 @@ export default function MainTask() {
         <p>Manage your tasks and stay productive</p>
       </div>
       <div className="input-wrap">
-        <input type="text" placeholder="what need to be done" />
-        <button className="btn-submit">Add Tasks</button>
+        <input
+          type="text"
+          placeholder="what need to be done"
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value);
+
+            console.log(e);
+          }}
+        />
+        <button
+          className="btn-submit"
+          onClick={(e) => {
+            setValue("");
+            console.log(arr, value);
+            setArr([...arr, value]);
+            setValue("");
+          }}
+        >
+          Add Tasks
+        </button>
       </div>
       <div className="all-to-do">
         <ToDoList array={arr} />
